19d290eb53717e322e32c5ff4420fd6cb8680c80
Shebin John Samuel <shebinjs@users.noreply.github.com>
2016-09-19 22:32:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One - Shebin John Samuel',
+    heading: 'Article One',
+    date: '18 September 2016',
+    content: `<p>
+                This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai. This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai.This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai.This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai.
+            </p>
+            <p>
+                This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai. This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai.This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai.This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai.
+            </p>
+            <p>
+                This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai. This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai.This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai.This is the content for this article. I am learning how to write an html page and getting its bits and pieces from Tanmai.
+            </p>`
+}
+
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate = `
+        <!doctype html>
+        <html>
+            <head>
+                <link href="/ui/style.css" rel="stylesheet" />
+                <title>
+                    ${title}
+                </title>
+                <meta name="viewpart" content="width-device-width, initial-scale=1"/>
+            </head>
+            <body>
+                <div class="fullBody">
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    <hr/>
+                    <h3>
+                        ${heading}
+                    </h3>
+                    <div>
+                        ${date}
+                    </div>
+                    ${content}
+                </div>
+            </body>
+        </html>
+        `;
+        return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function(req, res) {
